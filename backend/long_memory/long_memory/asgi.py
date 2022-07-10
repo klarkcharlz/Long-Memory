444269721f4630082d1dd9bafa9494859176fcfe
Nikoilay Petrov <klark.charlz@mail.ru>
2022-07-10 04:28:43 +0300

    creating a project base

---
+++ backend/long_memory/long_memory/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for long_memory project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'long_memory.settings')
+
+application = get_asgi_application()
---
+++ backend/long_memory/long_memory/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for long_memory project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'long_memory.settings')
+
+application = get_wsgi_application()
