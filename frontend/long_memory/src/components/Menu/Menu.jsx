cbed24e63daedaa533cca570aced71d6d50cb444
Andrey <dorst.andreas@gmail.com>
2022-07-17 09:48:46 +0300

    add adaptive

--- frontend/long_memory/src/components/Menu/Menu.jsx
+++ frontend/long_memory/src/components/Menu/Menu.jsx
@@ -4,7 +4,7 @@ import classes from "./Menu.module.css";
 
 const Menu = () => {
     return (
-        <nav>
+        <nav className={classes.container}>
             <Link className={classes.link} to='/create_notification'><button className={classes.button}>Create<br/>memory<br/>card</button></Link>
             <Link className={classes.link} to='/notifications_list'><button className={classes.button}>Show<br/>my<br/>cards</button></Link>
         </nav>
