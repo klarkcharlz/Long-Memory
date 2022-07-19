05eec9f5a193d9263cbdf25ae6458803884cf56d
impreza555 <alex.erjomin@outlook.com>
2022-07-20 02:03:22 +0300

    Запилил CreateUserView.

--- backend/long_memory/users/views.py
+++ backend/long_memory/users/views.py
@@ -1,8 +1,11 @@
 from rest_framework import permissions
 from rest_framework.generics import CreateAPIView
 from django.contrib.auth import get_user_model
-
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework import status
 from .serializers import UserSerializer
+from .models import CustomUser
 
 
 class CreateUserView(CreateAPIView):
@@ -11,3 +14,12 @@ class CreateUserView(CreateAPIView):
         permissions.AllowAny
     ]
     serializer_class = UserSerializer
+    queryset = CustomUser.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        token, created = Token.objects.get_or_create(user=serializer.instance)
+        return Response({'token': token.key}, status=status.HTTP_201_CREATED, headers=headers)
