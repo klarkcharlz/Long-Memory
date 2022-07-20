4e84c126a579ad4d6fa29d9c1b00eacc0b649185
Nikoilay Petrov <klark.charlz@mail.ru>
2022-07-20 14:46:03 +0300

    add email receiver

--- frontend/long_memory/src/components/NotificationsList/NotificationsList.jsx
+++ frontend/long_memory/src/components/NotificationsList/NotificationsList.jsx
@@ -3,19 +3,28 @@ import classes from "./NotificationsList.module.css";
 import useUserContext from "../../hooks/useUserContext";
 import {getUserNotifications} from "../../functions/api";
 import {formatDate} from "../../functions/utils";
+import {FixedSizeList} from "react-window";
 
 
-const testData =
+const testData = [
     {
         id: 1,
         title: 'Programming',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In facilisis pretium mauris vel mattis. Phasellus ut augue auctor, lacinia magna a, efficitur nibh. Donec eget euismod mi, eu ullamcorper urna. Donec rhoncus sit amet lacus nec vehicula. Nullam non rutrum mauris. Ut convallis tortor sed est imperdiet, sed tristique risus dignissim. Praesent blandit rhoncus magna, a vulputate massa sagittis sit amet. Proin vitae porttitor turpis. Nam in molestie erat.',
         created_at: new Date().toString(),
         next_notifications: new Date().toString(),
-    }
+    }]
+
+const repeated = (id) => {
+    console.log('Повторил > ', id);
+}
+
+const disable = (id) => {
+    console.log('Больше не показывать > ', id);
+}
 
 const Notification = ({notification}) => {
-    const {title, description, created_at, next_notifications} = notification;
+    const {title, description, created_at, next_notifications, id} = notification;
     return (
         <div className={classes.container}>
             <div className={classes.notification_container}>
@@ -25,8 +34,16 @@ const Notification = ({notification}) => {
                 <p>напоминание: {formatDate(next_notifications)}</p>
             </div>
             <div className={classes.button}>
-                <button>Повторил</button>
-                <button>Больше не показывать</button>
+                <button onClick={(e) => {
+                    e.preventDefault();
+                    repeated(id);
+                }}>Повторил
+                </button>
+                <button onClick={(e) => {
+                    e.preventDefault();
+                    disable(id);
+                }}>Больше не показывать
+                </button>
             </div>
         </div>
     )
@@ -42,20 +59,35 @@ const NotificationList = () => {
         }
     }, [token]);
 
-    if (notifications.length !== 0) {
-        return (
-            <div>
-                <h2>Ваши напоминания</h2>
-                {notifications.map((notification) =>
-                    <Notification notification={notification}
-                                  key={notification.id}
-                    />)
-                }
-            </div>
-        )
+    const NotificationListRaw = () => {
+        if (notifications.length !== 0) {
+            return (
+                <div>
+                    <h2>Ваши напоминания</h2>
+                    {notifications.map((notification) =>
+                        <Notification notification={notification}
+                                      key={notification.id}
+                        />)
+                    }
+                </div>
+            )
+        }
+        return <h2>У вас пока не создано ни одного напоминания.</h2>
     }
-    return <h2>У вас пока не создано ни одного напоминания.</h2>
 
+    // return (
+    //     <FixedSizeList
+    //         height={window.innerHeight}
+    //         width={window.innerWidth / 2}
+    //         itemCount={notifications.length}
+    //         itemSize={25}
+    //     >
+    //         {NotificationListRaw}
+    //     </FixedSizeList>
+    // )
+
+    return NotificationListRaw();
 }
+
 
 export default NotificationList;
