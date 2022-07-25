6cd097483b06263c782d12d55ea045932fde69b7
KstanislavK <t7s9SF2FWTQfXxL>
2022-07-25 17:48:26 +0300

    Email v0.3

--- notification_system/mail/email_sender.py
+++ notification_system/mail/email_sender.py
@@ -1,6 +1,7 @@
+import os
 import ssl
 import smtplib
-import time
+from dotenv import load_dotenv
 
 import jinja2
 
@@ -8,26 +9,22 @@ from datetime import datetime
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-import schedule
-from notifications.models import Notifications
-from users.models import CustomUser
 
-
-def send_email(sender, password, mail_add, username, body):
+def send_email(sender, password, domain, mail_add, name, body):
     """
     Отправляет письмо по параметрам
     :param sender: почта сайта
     :param password: пароль от почты
+    :param domain: домен почты
     :param mail_add: адрес получателя
-    :param username: имя получателя
+    :param name: имя получателя
     :param body: тело письма
     :return:
     """
 
-    domain = f"smtp.{sender.split('@')[1]}"  # домен нашей почты
     msg = MIMEMultipart()
     msg['From'] = 'Long Memory App'
-    msg['To'] = username
+    msg['To'] = name
     msg['Subject'] = f'Пора повторять материал!'
     msg.attach(MIMEText(body, 'html'))
 
@@ -40,50 +37,81 @@ def send_email(sender, password, mail_add, username, body):
         print(e)
 
 
-def get_body(username, notifications):
+def get_body(name, notifications):
     """
     jinja2
-    :param username: имя пользователя (получателя)
+    :param name: имя пользователя (получателя)
     :param notifications: активные напоминания пользователя
     :return:
     """
     loader = jinja2.FileSystemLoader('templates/email')  # загружаем папку с шаблоном
     j_env = jinja2.Environment(loader=loader)
     content = {
-        'username': username,
+        'name': name,
         'notifications': notifications,
     }
-    tpl = j_env.get_template('email.html')
+    tpl = j_env.get_template('email_body.html')
     return tpl.render(content)
 
 
-def send_for_user():
+def send_for_user(data_set):
     """
-    определяет активных юзеров
-            грузим его активные нотифы
-        отправляем инфу на формирование тела письма, получаем тело
-        отдаем тело и параметры на отправку
+    Раскладываем по полкам полученный массив данных из очереди относительно юзера
+    Формируем тело письма
+    Передаем на отправку
+
+    :param data_set: массив данных из очереди
     """
-    sender = ''  # надо доставать из env
-    password = ''  # надо доставать из env
-    users = CustomUser.objects.filter(is_active=True)
-    for user in users:
-        notifications = Notifications.objects.filter(user_id=user.pk,
-                                                     is_active=True,
-                                                     next_notification__lte=datetime.now())
-        mail_add = user.email
-        username = user.username
-        body = get_body(username, notifications)
-        send_email(sender, password, mail_add, username, body)
+    load_dotenv()
+    sender = os.getenv('SENDER')
+    password = os.getenv('PASSWORD')
+    domain = os.getenv('DOMAIN')
+    for item in data_set:
+        if len(item['notifications']) > 0:
+            email_add = item['email']
+            name = item['name']
+            notifications = item['notifications']
+            body = get_body(name, notifications)
+            send_email(sender, password, domain, email_add, name, body)
+
+            # Если напоминаний нет, нужна ли заглушка, типа
+            # "вам нечего повторять сегодня, отдохните или начните изучать что-то новое
+            # или почитайте статьи на нашем сайте"
+            # Или просто письмо не отправлять?
+
+    print(f'[INFO] {len(data_set)} messages sent')
 
 
-schedule.every().day.at("10:00").do(send_for_user)  # запуск скрипта каждый день в 10.00
+if __name__ == '__main__':
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)  # оставлено в целях тестирования
-
-
-
-# Естественно будет дорабатываться, нужно изучить Rabbit, вопрос с необходимостью SSL сертификата
-# Временно перевел на schedule
+    data = [
+        {'email': 'stanislav.afk@gmail.com',
+         'name': 'SuperMan',
+         'notifications': [
+             {'title': 'React Hooks',
+              'description': 'UseState',
+              'created_at': datetime.now(),
+              'next_notification': datetime.now(),
+              },
+             {'title': 'React Hooks',
+              'description': 'useEffect',
+              'created_at': datetime.now(),
+              'next_notification': datetime.now(),
+              }
+         ]},
+        # {'email': 'skkolenov@gmail.com',
+        #  'name': 'Mike',
+        #  'notifications': [
+        #      {'title': 'Python',
+        #       'description': 'lambda function',
+        #       'created_at': datetime.now(),
+        #       'next_notification': datetime.now(),
+        #       },
+        #      {'title': 'JavaScript',
+        #       'description': 'work with date',
+        #       'created_at': datetime.now(),
+        #       'next_notification': datetime.now(),
+        #       }
+        #  ]},
+    ]
+    send_for_user(data)
