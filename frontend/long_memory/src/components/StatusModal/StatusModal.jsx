9c91fb54f2fa692995674c5e8c40a3ee5e341fb2
Your Name <you@example.com>
2022-07-27 16:22:27 +0400

    update style modal and PersonalArea

--- frontend/long_memory/src/components/StatusModal/StatusModal.jsx
+++ frontend/long_memory/src/components/StatusModal/StatusModal.jsx
@@ -5,34 +5,39 @@ import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    color: '#000',
+    bgcolor: 'white',
+    // boxShadow: '5px 5px 100px rgba(61,61,61,0.5)',
+    border: '6px inset #08baad',
+    borderRadius: '25px',
+    opacity: '0.9',
+    // boxShadow: 24,
+    textAlign: 'center',
+    p: 4,
 };
 
 export default function StatusModal({open, setOpen, status}) {
-  const handleClose = () => setOpen(false);
+    const handleClose = () => setOpen(false);
 
-  return (
-    <div>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            {status}
-          </Typography>
-        </Box>
-      </Modal>
-    </div>
-  );
+    return (
+        <div>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        {status}
+                    </Typography>
+                </Box>
+            </Modal>
+        </div>
+    );
 }
