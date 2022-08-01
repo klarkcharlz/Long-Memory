7b0d4fe146b75ef579e67feb0d36bf9bfaab2857
wwfirsov <wwfirsov@mail.ru>
2022-08-01 14:46:01 +0300

    test_1

--- backend/long_memory/users/tests.py
+++ backend/long_memory/users/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from rest_framework import status
+from django.urls import reverse
+from rest_framework.test import APITestCase, APIClient
+from users.models import CustomUser
 
-# Create your tests here.
+
+class ChekUser(APITestCase):
+
+    url = "/admin/"
+    username = 'admin'
+    password = '12345'
+
+    def setUp(self) -> None:
+        self.user = CustomUser.objects.create_user(
+            username = self.username,
+            password = self.password,
+        )
+        self.client = APIClient()
+
+
+    def test_auth(self):
+
+        response = self.client.get(reverse('admin:index'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
