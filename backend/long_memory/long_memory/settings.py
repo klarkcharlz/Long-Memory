4caeefbf650e018da5f3522059da7161ea105a55
Nikoilay Petrov <klark.charlz@mail.ru>
2022-08-08 13:24:11 +0300

    add allowed hosts

--- backend/long_memory/long_memory/settings.py
+++ backend/long_memory/long_memory/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'longmemory.ru']
 
 # Application definition
 
