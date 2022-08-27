1c03e9821244de607dc7efc3f8e0bdd7acb1a8df
Nikoilay Petrov <klark.charlz@mail.ru>
2022-08-28 01:32:08 +0300

    fix next notification date

--- backend/long_memory/notifications/models.py
+++ backend/long_memory/notifications/models.py
@@ -6,13 +6,17 @@ from users.models import CustomUser
 from .constants import TIME_DELTA_MAP
 
 
+def utc_tomorrow():
+    return datetime.utcnow() + timedelta(days=1)
+
+
 class Notifications(models.Model):
     user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     title = models.CharField(max_length=64, verbose_name="Заголовок")
     description = models.TextField(verbose_name="Описание")
     period_type = models.SmallIntegerField(default=0)
     next_notifications = models.DateTimeField(verbose_name="Дата следующего напоминания",
-                                              default=datetime.utcnow() + timedelta(days=1))
+                                              default=utc_tomorrow)
     is_active = models.BooleanField(verbose_name="Описание", default=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата регистрации')
 
