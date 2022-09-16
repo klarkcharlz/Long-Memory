6087b39a4bac99815381eadc7f0050639f752568
Nikoilay Petrov <klark.charlz@mail.ru>
2022-09-16 15:45:43 +0300

    fix settings database connect

--- backend/long_memory/settings/common.py
+++ backend/long_memory/settings/common.py
@@ -163,3 +163,14 @@ DJOSER = {
     'SEND_ACTIVATION_EMAIL': True,
     'SERIALIZERS': {},
 }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'long_memory_db',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'db',  # для локального запуска localhost, для контейнера db
+        'PORT': '5432',  # для локального запуска 54326, для контейнера 5432
+    }
+}
--- backend/long_memory/settings/prod.py
+++ backend/long_memory/settings/prod.py
@@ -8,17 +8,6 @@ config = dotenv_values(".env")
 
 DEBUG = False
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'long_memory_db',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'HOST': 'db',  # для локального запуска localhost, для контейнера db
-        'PORT': '5432',  # для локального запуска 54326, для контейнера 5432
-    }
-}
-
 sentry_sdk.init(
     dsn=config['SENTRY_DSN'],
     integrations=[DjangoIntegration()],
