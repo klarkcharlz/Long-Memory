8eab4e93f180a97376e6ef4c3906d4e661e5ee9d
Nikoilay Petrov <klark.charlz@mail.ru>
2022-09-16 15:57:44 +0300

    costumize users admin panel

--- backend/long_memory/users/admin.py
+++ backend/long_memory/users/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 from .models import CustomUser
 
 
-admin.site.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username',
+                    'email',
+                    'telegram_id',
+                    'vk_id',
+                    'created_at',
+                    'is_active')
+    list_display_links = ('username', 'email')
+    search_fields = ('username', 'email')
+    list_filter = ('is_active', )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
