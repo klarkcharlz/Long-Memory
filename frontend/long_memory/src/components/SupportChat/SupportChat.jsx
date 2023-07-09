98055247d9ff09d1d31ac2e5a10cbfe7c47d8702
Nikoilay Petrov <klark.charlz@mail.ru>
2023-07-10 01:47:15 +0300

    add raw support chat component

--- frontend/long_memory/src/components/SupportChat/SupportChat.jsx
+++ frontend/long_memory/src/components/SupportChat/SupportChat.jsx
@@ -26,6 +26,92 @@ ws.onmessage = (event) => {
 };
 
 
+const testData = [
+    {
+        writer: 'user',
+        message: 'Здарова'
+    },
+    {
+        writer: 'admin',
+        message: 'чо случилось'
+    },
+    {
+        writer: 'user',
+        message: 'твой сайт гавно'
+    },
+    {
+        writer: 'admin',
+        message: 'я знаю'
+    },
+]
+const Chat = () => {
+    return (
+        <div style={{
+            width: '300px',
+            height: '400px',
+            backgroundColor: '#092754',
+            border: '2px solid #CFB82E',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            borderRadius: '10px',
+        }}>
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+            }}>
+                <input style={{
+                    height: '25px',
+                    borderTopLeftRadius: '10px',
+                    borderTopRightRadius: '10px',
+                    padding: '3px'
+                }} type="text" placeholder='Ваше имя'/>
+            </div>
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+            }}>
+                {testData.map((msg, index) => {
+                    return <div style={{
+                        alignSelf: msg.writer === 'user' ? 'end' : 'start',
+                        border: '1px solid #CFB82E',
+                        color: 'white',
+                        margin: '5px',
+                        padding: '3px',
+                        borderRadius: '10px'
+                    }}>
+                        <p style={{
+                            textAlign: msg.writer === 'user' ? 'right' : 'left',
+                            color: 'gray'
+                        }}>{msg.writer}</p>
+                        <p>{msg.message}</p>
+                    </div>
+                })}
+            </div>
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+            }}>
+                <input style={{
+                    height: '25px',
+                    padding: '3px'
+                }} type="text" placeholder='Сообщение'/>
+                <button style={{
+                    background: 'transparent',
+                    borderBottomLeftRadius: '10px',
+                    borderBottomRightRadius: '10px',
+                    color: 'white',
+                    opacity: 0.8,
+                    height: '35px',
+                    marginTop: '5px'
+                }}>отправить
+                </button>
+            </div>
+        </div>
+    );
+};
+
+
 const SupportChat = () => {
     const [name, setName] = useState('');
     const [message, setMessage] = useState('');
@@ -36,17 +122,19 @@ const SupportChat = () => {
     return (
         <div>
             <form action="">
-                <input type="text" placeholder='Ваше имя' value={name} onChange={
-                    (e) => {
-                        setName(e.target.value);
-                    }
-                }/>
+                <input type="text" placeholder='Ваше имя' value={name}
+                       onChange={
+                           (e) => {
+                               setName(e.target.value);
+                           }
+                       }/>
                 <br/>
-                <input type="text" placeholder='Сообщение' value={message} onChange={
-                    (e) => {
-                        setMessage(e.target.value);
-                    }
-                }/>
+                <input type="text" placeholder='Сообщение' value={message}
+                       onChange={
+                           (e) => {
+                               setMessage(e.target.value);
+                           }
+                       }/>
                 <br/>
                 <button onClick={(e) => {
                     e.preventDefault();
@@ -65,7 +153,9 @@ const SupportChat = () => {
                 }}>Отправить
                 </button>
             </form>
-            <textarea name="text" id="" cols="30" rows="10" value={answer} readonly={true}></textarea>
+            <textarea name="text" id="" cols="30" rows="10" value={answer}
+                      readonly={true}></textarea>
+            <Chat/>
         </div>
     );
 };
--- frontend/long_memory/src/index.js
+++ frontend/long_memory/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
+// import * as Sentry from "@sentry/react";
+// import { BrowserTracing } from "@sentry/tracing";
 import {UserProvider} from "./components/UserProvider/UserProvider";
 import { SENTRY_DSN } from './functions/api_constants';
 
