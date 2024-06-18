b93717461bcdfc706515fd0d577f845a08af841f
Nik Per <klark.charlz@mail.ru>
2024-06-18 08:20:37 +0300

    fix/http-over-https

--- backend/long_memory/settings/prod.py
+++ backend/long_memory/settings/prod.py
@@ -7,6 +7,10 @@ from .common import *
 config = dotenv_values(".env")
 
 DEBUG = False
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+MEDIA_URL = 'https://longmemory.ru/media/'
+STATIC_URL = 'https://longmemory.ru/static/'
 
 sentry_sdk.init(
     dsn=config['SENTRY_DSN'],
