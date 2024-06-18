855428f5a28a787a0a6bc0ef2fcfd303f50f1a67
Nik Per <klark.charlz@mail.ru>
2024-06-18 08:36:08 +0300

    rm-yandex-metrica

--- backend/long_memory/settings/prod.py
+++ backend/long_memory/settings/prod.py
@@ -11,7 +11,9 @@ SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 MEDIA_URL = 'https://longmemory.ru/media/'
 STATIC_URL = 'https://longmemory.ru/static/'
-
+SESSION_COOKIE_SAMESITE = 'Lax'  # Или 'Strict', в зависимости от ваших требований
+CSRF_COOKIE_SAMESITE = 'Lax'
+    
 sentry_sdk.init(
     dsn=config['SENTRY_DSN'],
     integrations=[DjangoIntegration()],
